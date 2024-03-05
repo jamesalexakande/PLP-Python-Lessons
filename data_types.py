0f16fdd721966e7dc15e230a7b86ce374c00b7ea
James Alex Akande <jamesakande007@gmail.com>
2024-03-05 10:15:07 +0100

    My PLP Python experiments

---
+++ data_types.py
@@ -0,0 +1,44 @@
+#Data Types in Python
+a=33
+b=3.51
+c=23+5j
+#print('The type of variable having value', a, 'is', type(a))
+#print('The type of variable having value', b, 'is', type(b))
+#print('The type of variable having value', c, 'is', type(c))
+#print(a+b)
+#print(a*b-c)
+d=False
+#print(d)
+#print('The type of variable having value', d, 'is', type(d))
+new="Welcome To PLP Academy"
+#print(new)
+#print(new[0])
+#print(new[-1])
+plp='''Python Class'''
+#print(plp)
+#print(plp + ' ',new)
+#print(new*4)
+New_List = ['bread','milk','eggs',50, 3.5, 20+8j] 
+#print(New_List)
+friends=['Daniel', 'Gift', 'Esther', 'Gift', 22, 3.142, 30+5j]
+#print(friends)
+#print(New_List[0])
+#print(New_List[3:5])
+#print(New_List + friends)
+jamesplp_tuple = 'Daniel', 'Gift', 'Esther', 'Gift', 22, 3.142, 30+5j
+#print(jamesplp_tuple) 
+tiny_tuple = ('John', 'James', 23*9, 5+7j)
+#print(tiny_tuple)
+#print(jamesplp_tuple + tiny_tuple)
+#print(tiny_tuple*4)
+dict = {}
+My_Dict={1:'one', 2:'two', 3:'three', 4:'four'}
+print('My Dict is', type(My_Dict))
+Dict_Two = {'name': 'James', 'type': 'human', 'number':35557, 'dept':522}
+dict['one'] = 'Tuple is my name'
+#You can call for items in a dictionary via square brackets
+print(dict['one'])
+print(My_Dict)
+print(My_Dict.keys)#This it an error so WE CORRECT IT IN NEXT LINE
+print(My_Dict.keys())
+print(My_Dict.values())#extra parenthesis added just like keys, to prevent errors
---
+++ greetings.py
@@ -0,0 +1,4 @@
+print("Hello World, Welcome To Python!")
+
+# creating a comment is what I am doing
+print('This is my first code in Python')
